70e98eab481b6600d0ca6260d9907be2cf051e00
donly1234 <vividmemoriesfilms@gmail.com>
2026-01-24 00:41:24 -0600

    Create featureFlags.ts

---
+++ components/config/featureFlags.ts
@@ -0,0 +1,15 @@
+
+export const FEATURE_FLAGS = {
+  AI_STRATEGY: true,
+  HEATMAP_TOOL: true,
+  ADVANCED_ANALYTICS: true,
+  SENTIMENT_VOICE: true,
+  AGENCY_PROSPECTOR: true,
+  REQUEST_AUTOMATION: true // Toggle for the new automation engine
+};
+
+export type FeatureFlag = keyof typeof FEATURE_FLAGS;
+
+export const isFeatureEnabled = (flag: FeatureFlag): boolean => {
+  return FEATURE_FLAGS[flag];
+};
