1e50060f40c2f7e5f23f1435cc2b73bf183d3317
donly1234 <vividmemoriesfilms@gmail.com>
2026-01-27 23:11:59 -0600

    Update ReviewFeed.tsx

--- components/Dashboard/ReviewFeed.tsx
+++ components/Dashboard/ReviewFeed.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../../supabaseClient';
-import { logger } from '../../utils/logger';
+import { logger } from '../logger';
 
 interface ReviewFeedProps {
   isTrial?: boolean;
